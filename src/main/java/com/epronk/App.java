39ea46b12a49797a7dcba2f456a8ab5bdffeca2a
Eddy Pronk <epronk@muftor.com>
2023-07-28 13:50:48 +1000

    load

--- src/main/java/com/epronk/App.java
+++ src/main/java/com/epronk/App.java
@@ -1,5 +1,8 @@
 package com.epronk;
 
+import java.io.InputStream;
+import java.io.FileInputStream;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public void load(String filename)
+    {
+	try (InputStream input = new FileInputStream(filename)) {
+	    // do something useful
+	}
+	catch(Exception ex) {
+	    // handle the exception
+	}
     }
 }
